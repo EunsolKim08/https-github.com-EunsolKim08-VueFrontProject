33f03397f307c282b11dfb1f002c63cd6e8865fd
srec0072 <srec0072@sorincorp.com>
2023-02-23 10:33:48 +0900

    testCheck

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,112 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="black-bg">
+      <div id="board">
+        <h1>공지사항 게시판</h1>
+        <button id="writeButton" class="btnClass" v-on:click="getPopUpModal"> 글쓰기</button>
+        <button type="button" class="btnClass" v-on:click="fnDelete">삭제</button>
+        <table id="noticeBoard">
+          <thead>
+          <tr>
+            <th></th>
+            <th>No</th>
+            <th>제목</th>
+            <th>작성자</th>
+            <th>등록일시</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(row, noticeNo) in boardList" :key="noticeNo">
+            <td><input type="checkbox" class="chkBoxClass"></td>
+            <td>{{ row.noticeNo }}</td>
+            <td> <a>{{ row.noticeSj }}</a></td>
+            <td>{{ row.registerId }}</td>
+            <td>{{ row.registDt}}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <div id="modalPopUp" v-if="modalOpen === true">
+        <form>
+          <div class="btnCloseModal">
+             <button id="btnCloseModal" v-on:click="closePopUpModal">X</button>
+          </div>
+          <div class="board-contents">
+            <input id="noticeSj" type="text" v-model="noticeSj" placeholder="제목을 입력해주세요.">
+          </div>
+          <div class="board-contents">
+            <textarea id="noticeCn" cols="30" rows="10" v-model="noticeCn"  style="resize: none;">
+            </textarea>
+          </div>
+          <div class="common-buttons">
+            <button type="button" class="btnClass" v-on:click="fnSave">저장</button>&nbsp;
+            <button type="button" class="btnClass" v-on:click="fnDelete">삭제</button>
+          </div>
+        </form>     
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      boardList: [],
+      modalOpen: false,
+    }
+  },
+  mounted() {
+    this.load();
+  },
+  methods: {
+    load() {
+      axios.get('/vueApi/selectNoticeList/').then(res => { 
+        console.log(res.data);
+        this.boardList = res.data;
+      });
+    },
+    getPopUpModal(){
+      console.log("글쓰기 버튼 클릭");
+      this.modalOpen = true;
+    },
+    fnSave(){
+    
+      let url = "";
+
+      let params =  {
+          "noticeNo": this.noticeNo,
+          "noticeSj": this.noticeSj,
+          "noticeCn": this.noticeCn,
+          "noticeUpendexpsrAt": "N"
+      };
+      if(this.noticeNo == undefined){
+        url = "/vueApi/insertNoticeForm/";
+      }else{
+        url = "/vueApi/updatetNoticeForm/";
+      }
+
+      axios.post(url, params).then(res => { 
+        if(res.status == 200){
+          alert("저장이 완료되었습니다.");
+          this.modalOpen = false;
+        }else{
+          alert("저장을 실패했습니다. 잠시후 다시시도해 주세요.");
+        }    
+      });
+    },
+    fnDelete(){
+      console.log("삭제하기 버튼 클릭");
+    },
+    closePopUpModal(){
+      this.modalOpen = false;
+    },
+    components: {
+    
+    }
+  } 
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -52,7 +122,47 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-a {
-  color: #42b983;
+#board{
+  margin: 200px;
+}
+table {
+       width: 100%;
+       border: 1px solid #333333;
+       border-collapse: collapse;
+}
+td, thead, th{
+      padding: 10px;
+      border: 1px solid #333333;
+}
+#writeButton{
+
+  margin : 0px 10px 20px 75%;
+}
+.btnClass{
+  height: 30px;
+  width : 60px;
+}
+.black-bg{
+  //background:rgba(0,0,0,0.8);
+}
+.chkBoxClass{
+  background:rgba(0,0,0,0.8);
+}
+#modalPopUp{
+ 
+}
+#noticeSj{
+  width: 400px;
+  height: 30px;
+  font-size:18px;
+  margin-bottom: 10px;
+}
+#noticeCn{
+  width: 400px;
+  margin-bottom: 10px;
+  font-size:18px;
+}
+#btnCloseModal{
+  margin: 0px 0px 10px 350px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="modalPopUp">
+      <form>
+        <div class="board-contents">
+          <input id="noticeSj" type="text" v-model="noticeSj" placeholder="제목을 입력해주세요.">
+        </div>
+        <div class="board-contents">
+          <textarea id="noticeCn" cols="30" rows="10" v-model="noticeCn"  style="resize: none;">
+          </textarea>
+        </div>
+        <div class="common-buttons">
+          <button type="button" class="btnClass" v-on:click="fnSave">저장</button>&nbsp;
+          <button type="button" class="btnClass" v-on:click="fnDelete">삭제</button>
+        </div>
+      </form>     
+  </div>
+</template>
+<script>
+ export default {
+    name: 'modalPopUp',
+    components: {}
+  }
+</script>
